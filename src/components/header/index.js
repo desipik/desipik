e13db1637144d8efc5c8d458fb8112d3b29555ea
Abdul-Rafay Shaikh <me@arafays.com>
2017-09-08 18:11:27 +0500

    iniital header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,29 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
+import logoImage from '../../assets/logo.png';
+import {Row} from 'reactstrap';
 
 export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link activeClassName={style.active} href="/">Home</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
-				</nav>
+				<Row className='justify-content-end'>
+					<nav class={style.smallNav}>
+						<Link activeClassName={style.active} href="/careers">We're Hiring</Link>
+						<Link activeClassName={style.active} href="/about-us">About Us</Link>
+						<Link activeClassName={style.active} href="/login">Login</Link>
+						<Link class={style.signup}  activeClassName={style.active} href="/signup">Sign Up</Link>
+					</nav>
+				</Row>
+				<Row className='justify-content-start'>
+					<img src={logoImage} class={style.logo} alt="" />
+					<nav class={style.mainNav}>
+						<Link activeClassName={style.active} href="/">Home</Link>
+						<Link activeClassName={style.active} href="/profile">Me</Link>
+						<Link activeClassName={style.active} href="/profile/zakir">zakir</Link>
+					</nav>
+				</Row>
 			</header>
 		);
 	}
